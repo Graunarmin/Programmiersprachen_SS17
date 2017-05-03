0254c715f835c4d68c4ce9afefece95fb54cd730
Johanna Sacher <johanna.sacher@uni-weimar.de>
2017-05-03 17:41:36 +0200

    Add Vec2

---
+++ source/vec2.cpp
@@ -0,0 +1,66 @@
+#include "vec2.hpp"
+
+//Konstruktoren
+
+Vec2::Vec2():
+	x_{0}, 
+	y_{0} {}
+
+Vec2::Vec2(float x, float y):
+	x_{x}, 
+	y_{y} {}
+
+Vec2::Vec2(Vec2 const& v):
+	x_{v.x_},
+	y_{v.y_} {}
+
+
+//Operation overloading
+
+Vec2& Vec2::operator += (Vec2 const & v){
+	this -> x_ += v.x_;
+	this -> y_ += v.y_;
+	return *this; //this wird durch das * zum referenzierten Element
+}
+
+Vec2& Vec2::operator -= (Vec2 const & v){
+	this -> x_ -= v.x_;
+	this -> y_ -= v.y_;
+	return *this;
+
+}
+
+Vec2& Vec2::operator *= (float s){
+	this -> x_ *= s;
+	this -> y_ *= s;
+	return *this;
+}
+
+Vec2& Vec2::operator /= (float s){
+	this -> x_ /= s;
+	this -> y_ /= s;
+	return *this;
+}
+
+Vec2 const operator +(Vec2 const& u, Vec2 const& v){
+	return Vec2{u} += v; 
+}
+
+Vec2 const operator -(Vec2 const& u, Vec2 const& v){
+	return Vec2{u} -= v;
+}
+
+Vec2 const operator *(Vec2 const& v, float s){
+	return Vec2{v} *= s;
+}
+
+Vec2 const operator /(Vec2 const& v, float s){
+	return Vec2{v} /= s;
+}
+
+
+Vec2 const operator *(float s, Vec2 const& v){
+	return Vec2{v} *= s;
+}
+
+
